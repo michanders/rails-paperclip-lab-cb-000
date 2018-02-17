3f452f5969456f9673abca800506225081fc9e36
Michael Anderson <michanders@msn.com>
2018-02-17 21:35:11 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,5 @@
 class Artist < ActiveRecord::Base
   has_many :songs
+  has_attached_file :avatar, default_url: ':style/default.png', styles: { thumb: "100x100>" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
