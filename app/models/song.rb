afe51ad580ea9c72082cd5c80cfe22d0c49c5792
Michael Anderson <michanders@msn.com>
2018-02-17 21:35:26 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
   has_attached_file :avatar
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  has_attached_file :avatar, default_url: ':style/default.png', styles: { thumb: "100x100>" }
 
   def artist_name
     self.try(:artist).try(:name)
